ac53d2a4307abf8744aa4052b7363bfb0971b019
felkng <felipersf374@gmail.com>
2026-09-19 12:21:29 -0300

    feat(repo): create QuestionRepository
    Add Spring Data JPA repository for Question with pagination and JpaSpecificationExecutor.

---
+++ src/main/java/github/felkng/olha_minha_questao/domain/repository/QuestionRepository.java
@@ -0,0 +1,16 @@
+package github.felkng.olha_minha_questao.domain.repository;
+
+import github.felkng.olha_minha_questao.domain.entity.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Long>, JpaSpecificationExecutor<Question> {
+    Page<Question> findByYear(Integer year, Pageable pageable);
+    Page<Question> findByOriginId(Long originId, Pageable pageable);
+    Page<Question> findByAreaId(Long areaId, Pageable pageable);
+    Page<Question> findByTestId(Long testId, Pageable pageable);
+}
