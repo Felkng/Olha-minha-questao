1f0895fb671a6dd217c6b8ba73b61961bdce6851
felkng <felipersf374@gmail.com>
2026-09-19 13:03:06 -0300

    feat(repo): create FolderRepository
    Add Spring Data JPA repository for Folder with ordered query methods.

---
+++ src/main/java/github/felkng/olha_minha_questao/domain/repository/FolderRepository.java
@@ -0,0 +1,12 @@
+package github.felkng.olha_minha_questao.domain.repository;
+
+import github.felkng.olha_minha_questao.domain.entity.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FolderRepository extends JpaRepository<Folder, Long> {
+    List<Folder> findAllByOrderByNameAsc();
+}
