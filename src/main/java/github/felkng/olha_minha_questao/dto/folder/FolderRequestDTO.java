ec8310f377d68f12d7110e13d7ba44359fef578d
felkng <felipersf374@gmail.com>
2026-09-19 13:03:06 -0300

    feat(dto): create FolderRequestDTO
    Create request DTO for creating and updating study folders with validation.

---
+++ src/main/java/github/felkng/olha_minha_questao/dto/folder/FolderRequestDTO.java
@@ -0,0 +1,26 @@
+package github.felkng.olha_minha_questao.dto.folder;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FolderRequestDTO {
+
+    @NotBlank(message = "O nome da pasta é obrigatório")
+    @Size(max = 150, message = "O nome da pasta não pode exceder 150 caracteres")
+    private String name;
+
+    private String description;
+
+    @Builder.Default
+    private String color = "#d9b763";
+}
