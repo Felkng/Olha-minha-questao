0e18ad8be2458b49ba2da2c1d202ad4f3e54dcfe
felkng <felipersf374@gmail.com>
2026-09-19 13:45:50 -0300

    update(repository): add findAllByOrderByNameAsc to AreaRepository

--- src/main/java/github/felkng/olha_minha_questao/domain/repository/AreaRepository.java
+++ src/main/java/github/felkng/olha_minha_questao/domain/repository/AreaRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 public interface AreaRepository extends JpaRepository<Area, Long> {
     Optional<Area> findByNameIgnoreCase(String name);
     boolean existsByNameIgnoreCase(String name);
+    java.util.List<Area> findAllByOrderByNameAsc();
 }
