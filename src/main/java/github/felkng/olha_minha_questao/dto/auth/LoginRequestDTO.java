ede7d77133e8af48e17601244c47b9a8b47ea465
felkng <felipersf374@gmail.com>
2026-09-19 14:42:33 -0300

    create(dto): add LoginRequestDTO

---
+++ src/main/java/github/felkng/olha_minha_questao/dto/auth/LoginRequestDTO.java
@@ -0,0 +1,22 @@
+package github.felkng.olha_minha_questao.dto.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginRequestDTO {
+
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "Insira um e-mail válido")
+    private String email;
+
+    @NotBlank(message = "A senha é obrigatória")
+    private String password;
+}
