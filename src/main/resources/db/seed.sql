934e4dbb6a29e5c976b31295d58828873ab01514
felkng <felipersf374@gmail.com>
2026-09-19 14:46:23 -0300

    update(db): add initial users to seed.sql

--- src/main/resources/db/seed.sql
+++ src/main/resources/db/seed.sql
@@ -184,9 +184,16 @@ UPDATE test_statistic SET total_attempts = 18, average_score = 82.0, difficulty_
 -- Prova 3: 45.0% -> DIFICIL
 UPDATE test_statistic SET total_attempts = 22, average_score = 45.0, difficulty_level = 'DIFICIL' WHERE test_id = 3;
 
--- 9. Atualização das sequências das tabelas
+-- 9. Inserção de Usuários Padrão (ADMIN e GENERAL)
+INSERT INTO app_user (id, name, email, password_hash, role, created_at, updated_at) VALUES
+(1, 'Administrador', 'admin@olhaminhaquestao.com', '240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9', 'ADMIN', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(2, 'Fulano de Tal', 'fulano@olhaminhaquestao.com', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'GENERAL', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+ON CONFLICT (email) DO NOTHING;
+
+-- 10. Atualização das sequências das tabelas
 SELECT setval('origin_id_seq', (SELECT MAX(id) FROM origin));
 SELECT setval('area_id_seq', (SELECT MAX(id) FROM area));
 SELECT setval('test_id_seq', (SELECT MAX(id) FROM test));
 SELECT setval('question_id_seq', (SELECT MAX(id) FROM question));
 SELECT setval('alternative_id_seq', (SELECT MAX(id) FROM alternative));
+SELECT setval('app_user_id_seq', (SELECT MAX(id) FROM app_user));
