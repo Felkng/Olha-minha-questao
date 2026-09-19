af2771e5e68e64039f66ee3ea413a3d5d3cfbe6b
felkng <felipersf374@gmail.com>
2026-09-19 12:21:29 -0300

    feat(repo): create OriginRepository
    Add Spring Data JPA repository for Origin with case-insensitive search.

---
+++ src/main/java/github/felkng/olha_minha_questao/domain/repository/OriginRepository.java
@@ -0,0 +1,13 @@
+package github.felkng.olha_minha_questao.domain.repository;
+
+import github.felkng.olha_minha_questao.domain.entity.Origin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface OriginRepository extends JpaRepository<Origin, Long> {
+    Optional<Origin> findByNameIgnoreCase(String name);
+    boolean existsByNameIgnoreCase(String name);
+}
