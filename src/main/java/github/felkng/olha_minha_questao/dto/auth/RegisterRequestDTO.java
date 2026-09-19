0e7e61bbcd5d60a29bd8a616c408d9815108be7e
felkng <felipersf374@gmail.com>
2026-09-19 14:42:33 -0300

    create(dto): add RegisterRequestDTO

---
+++ src/main/java/github/felkng/olha_minha_questao/dto/auth/RegisterRequestDTO.java
@@ -0,0 +1,28 @@
+package github.felkng.olha_minha_questao.dto.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegisterRequestDTO {
+
+    @NotBlank(message = "O nome é obrigatório")
+    @Size(max = 100, message = "O nome não pode exceder 100 caracteres")
+    private String name;
+
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "Insira um e-mail válido")
+    private String email;
+
+    @NotBlank(message = "A senha é obrigatória")
+    @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres")
+    private String password;
+}
