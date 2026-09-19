ac56a8b275f1cf25518e5e386a5e4efd2cfe7066
felkng <felipersf374@gmail.com>
2026-09-19 12:21:30 -0300

    test(app): add context loads test
    Add Spring Boot context loading test.

---
+++ src/test/java/github/felkng/olha_minha_questao/OlhaMinhaQuestaoApplicationTests.java
@@ -0,0 +1,13 @@
+package github.felkng.olha_minha_questao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlhaMinhaQuestaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
