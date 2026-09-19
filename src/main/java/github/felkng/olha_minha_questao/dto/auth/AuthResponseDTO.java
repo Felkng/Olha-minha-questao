c7cc46297b77c3f2362983fba3cd5e4d46ea52bb
felkng <felipersf374@gmail.com>
2026-09-19 14:42:33 -0300

    create(dto): add AuthResponseDTO

---
+++ src/main/java/github/felkng/olha_minha_questao/dto/auth/AuthResponseDTO.java
@@ -0,0 +1,19 @@
+package github.felkng.olha_minha_questao.dto.auth;
+
+import github.felkng.olha_minha_questao.domain.entity.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthResponseDTO {
+    private String token;
+    private Long id;
+    private String name;
+    private String email;
+    private UserRole role;
+}
