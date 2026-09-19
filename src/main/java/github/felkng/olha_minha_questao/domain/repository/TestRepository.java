b8b1ad7c6e46e0fa23cda33ee180b333d8c3ae27
felkng <felipersf374@gmail.com>
2026-09-19 12:21:29 -0300

    feat(repo): create TestRepository
    Add Spring Data JPA repository for Test with filter queries by year, origin, and area.

---
+++ src/main/java/github/felkng/olha_minha_questao/domain/repository/TestRepository.java
@@ -0,0 +1,15 @@
+package github.felkng.olha_minha_questao.domain.repository;
+
+import github.felkng.olha_minha_questao.domain.entity.Test;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TestRepository extends JpaRepository<Test, Long> {
+    List<Test> findByYear(Integer year);
+    List<Test> findByOriginId(Long originId);
+    List<Test> findByAreaId(Long areaId);
+    List<Test> findByOriginIdAndYear(Long originId, Integer year);
+}
