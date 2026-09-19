1928d2bbe91763b433a34e331733ccf4bf7529c0
felkng <felipersf374@gmail.com>
2026-09-19 12:21:30 -0300

    feat(mapper): create QuestionMapper
    Add MapStruct mapper for Question with nested mappers and correct alternative fields.

---
+++ src/main/java/github/felkng/olha_minha_questao/mapper/QuestionMapper.java
@@ -0,0 +1,36 @@
+package github.felkng.olha_minha_questao.mapper;
+
+import github.felkng.olha_minha_questao.domain.entity.Question;
+import github.felkng.olha_minha_questao.dto.question.QuestionRequestDTO;
+import github.felkng.olha_minha_questao.dto.question.QuestionResponseDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring", uses = {OriginMapper.class, AreaMapper.class, TestMapper.class, AlternativeMapper.class})
+public interface QuestionMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "origin", ignore = true)
+    @Mapping(target = "area", ignore = true)
+    @Mapping(target = "test", ignore = true)
+    @Mapping(target = "images", ignore = true)
+    @Mapping(target = "correctAlternative", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    Question toEntity(QuestionRequestDTO dto);
+
+    @Mapping(target = "correctAlternativeId", source = "correctAlternative.id")
+    @Mapping(target = "correctAlternativeIdentifier", source = "correctAlternative.identifier")
+    QuestionResponseDTO toDTO(Question entity);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "origin", ignore = true)
+    @Mapping(target = "area", ignore = true)
+    @Mapping(target = "test", ignore = true)
+    @Mapping(target = "images", ignore = true)
+    @Mapping(target = "correctAlternative", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    void updateEntityFromDTO(QuestionRequestDTO dto, @MappingTarget Question entity);
+}
