932131d820bfd4d76f18cc50b0041c6acd20a958
felkng <felipersf374@gmail.com>
2026-09-19 12:21:30 -0300

    feat(mapper): create AreaMapper
    Add MapStruct mapper for Area entity and DTO conversions.

---
+++ src/main/java/github/felkng/olha_minha_questao/mapper/AreaMapper.java
@@ -0,0 +1,24 @@
+package github.felkng.olha_minha_questao.mapper;
+
+import github.felkng.olha_minha_questao.domain.entity.Area;
+import github.felkng.olha_minha_questao.dto.area.AreaRequestDTO;
+import github.felkng.olha_minha_questao.dto.area.AreaResponseDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface AreaMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    Area toEntity(AreaRequestDTO dto);
+
+    AreaResponseDTO toDTO(Area entity);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    void updateEntityFromDTO(AreaRequestDTO dto, @MappingTarget Area entity);
+}
