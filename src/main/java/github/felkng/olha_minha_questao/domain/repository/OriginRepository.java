a2e7cae8d97f452d8dc015f8bd13f58195869c33
felkng <felipersf374@gmail.com>
2026-09-19 13:45:50 -0300

    update(repository): add findAllByOrderByNameAsc to OriginRepository

--- src/main/java/github/felkng/olha_minha_questao/domain/repository/OriginRepository.java
+++ src/main/java/github/felkng/olha_minha_questao/domain/repository/OriginRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 public interface OriginRepository extends JpaRepository<Origin, Long> {
     Optional<Origin> findByNameIgnoreCase(String name);
     boolean existsByNameIgnoreCase(String name);
+    java.util.List<Origin> findAllByOrderByNameAsc();
 }
