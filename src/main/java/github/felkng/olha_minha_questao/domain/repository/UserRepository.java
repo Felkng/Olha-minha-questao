7c39bbd91276fc5fed4ea77760ebccae562d6431
felkng <felipersf374@gmail.com>
2026-09-19 14:42:33 -0300

    create(repository): add UserRepository

---
+++ src/main/java/github/felkng/olha_minha_questao/domain/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package github.felkng.olha_minha_questao.domain.repository;
+
+import github.felkng.olha_minha_questao.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
