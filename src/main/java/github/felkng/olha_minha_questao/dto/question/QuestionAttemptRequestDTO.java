f92bc94b8672030fb3fc8bff11cc1e5f245ee419
felkng <felipersf374@gmail.com>
2026-09-19 13:45:56 -0300

    add(dto): create QuestionAttemptRequestDTO

---
+++ src/main/java/github/felkng/olha_minha_questao/dto/question/QuestionAttemptRequestDTO.java
@@ -0,0 +1,17 @@
+package github.felkng.olha_minha_questao.dto.question;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class QuestionAttemptRequestDTO {
+    private Long selectedAlternativeId;
+    private Boolean isFirstAttempt;
+    private Integer timeSpentSeconds;
+    private String sessionId;
+}
