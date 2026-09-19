eb0739e4c5bd4a2edaf60aa15fb4d1addd01e89e
felkng <felipersf374@gmail.com>
2026-09-19 12:21:29 -0300

    feat(app): add main spring boot application entrypoint
    Provide main class with @SpringBootApplication annotation to bootstrap the application.

---
+++ src/main/java/github/felkng/olha_minha_questao/OlhaMinhaQuestaoApplication.java
@@ -0,0 +1,13 @@
+package github.felkng.olha_minha_questao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlhaMinhaQuestaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlhaMinhaQuestaoApplication.class, args);
+	}
+
+}
